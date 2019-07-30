761f6b1fdbcea7ff6409148527016f6de401d49f
huitou <3.riverside.swe@gmail.com>
2019-07-30 23:39:28 +0100

    fixing naming

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ export default [
   // CommonJS
   {
     input: 'src/main.js',
-    output: { file: 'lib/r-form-model.js', format: 'cjs', indent: false },
+    output: { file: 'lib/r-template.js', format: 'cjs', indent: false },
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
@@ -18,7 +18,7 @@ export default [
   // ES
   {
     input: 'src/main.js',
-    output: { file: 'es/r-form-model.js', format: 'es', indent: false },
+    output: { file: 'es/r-template.js', format: 'es', indent: false },
     external: [
       ...Object.keys(pkg.dependencies || {}),
       ...Object.keys(pkg.peerDependencies || {}),
