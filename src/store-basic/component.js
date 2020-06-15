d11879423fd34b6d4523cffa9148c17deb4c2958
huitou <3.riverside.swe@gmail.com>
2020-06-15 21:34:27 +0100

    n.9 - updating copyright note

--- src/store-basic/component.js
+++ src/store-basic/component.js
@@ -1,7 +1,7 @@
 /*
     Store Basic Component.
 
-    Copyright (c) 2019 Riverside Software Engineering Ltd. All rights reserved.
+    Copyright (c) 2019-2020 Riverside Software Engineering Ltd. All rights reserved.
 
     Licensed under the MIT License. See LICENSE file in the project root for full license information.
 */
--- src/store-basic/index.js
+++ src/store-basic/index.js
@@ -1,7 +1,7 @@
 /*
     Collected Store Basic.
 
-    Copyright (c) 2019 Riverside Software Engineering Ltd. All rights reserved.
+    Copyright (c) 2019-2020 Riverside Software Engineering Ltd. All rights reserved.
 
     Licensed under the MIT License. See LICENSE file in the project root for full license information.
 */
